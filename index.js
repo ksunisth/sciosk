e8db0aea4a90b7986d3faa30e362daff6f26244c
Sunisth Kumar <ksunisth@gmail.com>
2021-06-10 21:36:29 +0530

    add styles

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const _cliProgress = require('cli-progress');
 const qoa = require('qoa');
 const {log} = console;
 
-log(chalk.red("SUNISTH"));
+// log(chalk.red(" ▒█▀▀▀█ ▒█░▒█ ▒█▄░▒█ ▀█▀ ▒█▀▀▀█ ▀▀█▀▀ ▒█░▒█ "));
+// log(chalk.red(" ░▀▀▀▄▄ ▒█░▒█ ▒█▒█▒█ ▒█░ ░▀▀▀▄▄ ░▒█░░ ▒█▀▀█ "));
+// log(chalk.red(" ▒█▄▄▄█ ░▀▄▄▀ ▒█░░▀█ ▄█▄ ▒█▄▄▄█ ░▒█░░ ▒█░▒█"));
+log();
 log(chalk.bold("Welcome to Sunisth, the best npm package about Sunisth Kumar."));
 
 const begin = {
     type: 'confirm',
     query: 'Do you wanna answer questions about Sunisth to satisfy his ego?',
-    handle: 'update',
+    handle: 'type',
+    accept: 'y',
+    deny: 'n'
+};
+
+const again = {
+    type: 'confirm',
+    query: 'Want to try again?',
+    handle: 'type',
     accept: 'y',
     deny: 'n'
 };
@@ -33,7 +44,7 @@ const qs = [
     },
     {
         type: 'quiz',
-        query: 'What is Sunisth\'s favourite color?',
+        query: 'What is Sunisth\'s favourite colour?',
         handle :'a',
         answer: 'Blue',
         symbol: '>',
@@ -62,7 +73,7 @@ const qs = [
     },
     {
         type: 'quiz',
-        query: 'What genre of music does Sunisth listen to when he\'s programing?',
+        query: 'What genre of music does Sunisth listen to when he\'s programming?',
         handle: 'a',
         answer: 'Pop',
         symbol: '>',
@@ -122,49 +133,49 @@ const qs = [
     },
     {
         type: 'input',
-        query: '(True or False) Sunsith once got a cast for two months from football. [t/f]',
+        query: '(True or False)\nSunsith once got a cast for two months from football. [t/f]',
         handle: 'a',
         answer: 'f'
     },
     {
         type: 'input',
-        query: '(True or False) Sunsith uses a different password for every account. [t/f]',
+        query: '(True or False)\nSunsith uses a different password for every account. [t/f]',
         handle: 'a',
         answer: 'f'
     },
     {
         type: 'input',
-        query: '(True or False) Sunsith always has matcha in his dorm. [t/f]',
+        query: '(True or False)\nSunsith always has matcha in his dorm. [t/f]',
         handle: 'a',
         answer: 't'
     },
     {
         type: 'input',
-        query: '(True or False) Even though he has never been there, Sunisth says that his favourite place is New York. [t/f]',
+        query: '(True or False)\nEven though he has never been there, Sunisth says that his favourite place is New York. [t/f]',
         handle: 'a',
         answer: 't'
     },
     {
         type: 'input',
-        query: '(True or False) Sunsith once blacked out at a party in Pune. [t/f]',
+        query: '(True or False)\nSunsith once blacked out at a party in Pune. [t/f]',
         handle: 'a',
         answer: 'f'
     },
     {
         type: 'input',
-        query: '(True or False) While Sunsith loves politics, he has never actually voted. [t/f]',
+        query: '(True or False)\nWhile Sunsith loves politics, he has never actually voted. [t/f]',
         handle: 'a',
         answer: 't'
     },
     {
         type: 'input',
-        query: '(True or False) Sunisth can name the capital of every state in India. [t/f]',
+        query: '(True or False)\nSunisth can name the capital of every state in India. [t/f]',
         handle: 'a',
         answer: 't'
     },
     {
         type: 'input',
-        query: '(True or False) Sunsith has watched Friends back to back three times. [t/f]',
+        query: '(True or False)\nSunsith has watched Friends back to back three times. [t/f]',
         handle: 'a',
         answer: 't'
     },
@@ -180,28 +191,59 @@ const qs = [
     let conf = qoa.prompt([begin]);
     let result = await conf;
 
-    if (result.update) {
-        log('\n')
-        let used = [];
-
-        for (let i = 1; i <= 7; i++) {
-            let rand = Math.floor(Math.random() * qs.length);
-
-            if(!(rand in used)) {
-                log(chalk.whiteBright.bgRed('  Question', i, '/7:  '));
-                let question = qoa.prompt([qs[rand]]);
-                let answer = await question;
-
-                if (qs[rand].type == 'input') {
-                    log(qs[rand].answer);
-                }
-                log(answer);
-                log('\n');
-
-                used.push(rand);
-            } else {
-                i--;
-            }
-        }
+    if (result.type) {
+        round()
     }
 })();
+
+async function round(){
+    qoa.clearScreen();
+    log('\n')
+    let used = [];
+    let score = 0;
+
+    for (let i = 1; i <= 7; i++) {
+        let rand = Math.floor(Math.random() * qs.length);
+
+        if(!(rand in used)) {
+            log(chalk.whiteBright.bgRed('  Question', i, '/7:  '));
+            let question = qoa.prompt([qs[rand]]);
+            let answer = await question;
+
+            if (qs[rand].type === 'input') {
+                if (qs[rand].answer === answer.a) {
+                    log(chalk.green('Correct'));
+                    score++;
+                } else {
+                log(chalk.red('Wrong'));
+                let longate = {t: 'true', f: 'false'};
+                if (longate[qs[rand].answer]) {
+                    log(chalk.gray('The correct answer is', longate[qs[rand].answer]));
+                } else {
+                    log(chalk.gray('The correct answer is', qs[rand].answer));
+                }
+            }
+            } else if (qs[rand].type === 'quiz') {
+                if (answer.a.isCorrect) {
+                    log(chalk.green('Correct'));
+                    score++;
+                } else {
+                    log(chalk.red('Wrong'));
+                    log(chalk.gray(`The correct answer is ${qs[rand].answer}`));
+                }
+            }
+            log('\n');
+
+            used.push(rand);
+        } else {
+            i--;
+        }
+    }
+    log(chalk.whiteBright.bgRed.bold('  You got', score, 'correct  '));
+    let another = qoa.prompt([again]);
+    let res = await another;
+
+    if (res.type) {
+        round();
+    }
+}
